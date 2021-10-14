1802dd3b127e8a8e7a156a28670611ed360b7fe1
Jordi Bonastre <jordi.bonastre.m@gmail.com>
2021-10-14 19:23:56 +0200

    added generateHtml function to Series class

--- public/Series.js
+++ public/Series.js
@@ -1,7 +1,21 @@
-class Series extends{
-  constructor(parentElement){
-    super(parentElement,"series","section");
-  } 
+import Componente from "./Componente";
 
+class Series extends Componente {
+  constructor(parentElement) {
+    super(parentElement, "series", "section");
+    this.generateHtml();
+  }
 
+  generateHtml() {
+    this.element.innerHTML = `
+        <section class="series">
+          <h2 class="section-title">Series list</h2>
+          <i class="icon--add fas fa-plus-circle"></i>
+          <section class="series-pending">
+          </section>
+          <section class="series-watched">
+          </section>
+        </section
+    `;
+  }
 }
