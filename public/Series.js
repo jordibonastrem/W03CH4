c3d73ffaede6cd31ff9be4bb888845d23ffecc8d
Jordi Bonastre <jordi.bonastre.m@gmail.com>
2021-10-14 20:36:42 +0200

    create serie and generate html function

--- public/Series.js
+++ public/Series.js
@@ -11,11 +11,25 @@ class Series extends Componente {
         <section class="series">
           <h2 class="section-title">Series list</h2>
           <i class="icon--add fas fa-plus-circle"></i>
+
           <section class="series-pending">
+            <h3 class="subsection-title">Pending series</h3>
+            <p class="info">You have 4 series pending to watch</p>
+            <!--<p class="info">Congrats! You've watched all your series</p>-->
+            <ul class="series-list">
+            
+            </ul>
           </section>
+
           <section class="series-watched">
+            <h3 class="subsection-title">Watched series</h3>
+            <p class="info">You have watched 4 series</p>
+            <!--<p class="info">You already have not watched any serie</p>-->
+            <ul class="series-list series-list--watched">
+            
+            </ul>
           </section>
-        </section
+        </section>
     `;
   }
 }
