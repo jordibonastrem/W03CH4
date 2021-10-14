0883e03ccdfc3b7a231b126d48cc176113ea79fd
Menchu MV <mcmvillalobos@gmail.com>
2021-10-14 20:02:35 +0200

    Added new Page to index.js

---
+++ public/index.js
@@ -0,0 +1,8 @@
+import Componente from "./Componente.js";
+import Form from "./Form.js";
+import Series from "./Series.js";
+import Page from "./Page.js";
+
+const body = document.querySelector("body");
+
+new Page(body);
