cbdd756fa9317b38a618d7a2a9aefb4b3053d92e
Menchu MV <mcmvillalobos@gmail.com>
2021-10-14 19:58:34 +0200

    Removed html from index

---
+++ public/Page.js
@@ -0,0 +1,23 @@
+import Componente from "./Componente.js";
+import Form from "./Form.js";
+import Series from "./Series.js";
+
+class Page extends Componente {
+  constructor(parentElement) {
+    super(parentElement, "container");
+
+    this.generateHTML();
+  }
+
+  generateHTML() {
+    this.element.innerHTML = `    <div class="container">
+      <header class="main-header">
+        <h1 class="main-title">My Series</h1>
+      </header>
+      <main class="main">
+      </main>
+    </div>`;
+  }
+}
+
+export default Page;
